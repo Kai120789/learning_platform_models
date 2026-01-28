95b2a9a5af61211301032a1c47ddaf456c8d2298
Kai120789 <zoomer120789@gmail.com>
2026-01-28 03:19:00 +0300

    fix: enums

--- migrations/003_create_user_settings.up.sql
+++ migrations/003_create_user_settings.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_settings (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id) NOT NULL,
-    is_2fa_enabled BOOLEAN NOT NULL,
-    is_notifications_enabled BOOLEAN NOT NULL
+    is_2fa_enabled BOOLEAN DEFAULT false NOT NULL,
+    is_notifications_enabled BOOLEAN DEFAULT false  NOT NULL
 );
--- migrations/007_create_user_info.up.sql
+++ migrations/007_create_user_info.up.sql
@@ -1,15 +1,15 @@
-CREATE TYPE role_enum AS ENUM ('admin', 'tutor', 'student');
+CREATE TYPE role_enum AS ENUM ('ADMIN', 'TUTOR', 'STUDENT');
 
-CREATE TYPE status_enum AS ENUM ('active', 'inactive', 'banned');
+CREATE TYPE status_enum AS ENUM ('ACTIVE', 'INACTIVE', 'BANNED');
 
 CREATE TABLE IF NOT EXISTS user_info (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id) NOT NULL,
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    about VARCHAR(1000) NOT NULL,
+    lastname VARCHAR(50),
+    city VARCHAR(50),
+    about VARCHAR(1000),
     role role_enum,
     status status_enum,
     class INTEGER
